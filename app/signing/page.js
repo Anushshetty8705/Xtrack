394606ca4baa5c602c0eb67641b055eceba63a1a
Anushshetty8705 <anushshetty123456@gmail.com>
2025-06-04 13:05:48 +0530

    Add files via upload

---
+++ app/signing/page.js
@@ -0,0 +1,9 @@
+import SignPage from './SignPage';
+export const metadata = {
+  title: "Xtrack-SigningPage",
+  description: "tarck your expense",
+  icons: [],
+};
+export default function SigningPage() {
+  return <SignPage />;
+}
