7e3c5cdf04297628d6d77bf8924c148f02a5dc5e
Anushshetty8705 <anushshetty123456@gmail.com>
2025-06-07 17:04:39 +0530

    app directory

--- app/signing/page.js
+++
@@ -1,9 +0,0 @@
-import SignPage from './SignPage';
-export const metadata = {
-  title: "Xtrack-SigningPage",
-  description: "tarck your expense",
-  icons: [],
-};
-export default function SigningPage() {
-  return <SignPage />;
-}
