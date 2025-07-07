22e0d34f096434388f67db862115d7e403f5604e
Anushshetty8705 <anushshetty123456@gmail.com>
2025-07-07 13:12:04 +0530

    Add files via upload

---
+++ app/forgotpassword/page.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Phonenumber from './Phonenumber'
+export const metadata = {
+  title: "Xtrack-Changing Password",
+  description: "tarck your expense",
+    icons: {
+    icon: [
+      
+      { url: '/logo.png', sizes: '16x16', type: 'image/png' },
+    ],
+  },
+}
+const page = () => {
+  return (
+    <>
+    <Phonenumber />
+    </>
+  )
+}
+
+export default page
